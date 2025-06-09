58b5ed62540d99ca0e254702e8392b610ae642d7
bramasta <ghastbad@gmail.com>
2025-06-09 13:23:07 +0700

    gunakan project ini untuk tubes

---
+++ src/main/java/org/pckg/buybayproject/BuyBayProjectApplication.java
@@ -0,0 +1,15 @@
+package org.pckg.buybayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyBayProjectApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Starting BuyBayProjectApplication");
+		SpringApplication.run(BuyBayProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pckg/buybayproject/BuyBayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pckg.buybayproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyBayProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
